6299e0601ab35ab697c04db93c8fb26b12b21838
Sam Estep <sam@samestep.com>
2024-01-15 14:50:16 -0500

    Use Rose v0.4.6 to implement Minkowski sum

--- triangles.ts
+++ triangles.ts
@@ -2,6 +2,7 @@
 
 import {
   Bool,
+  Nat,
   Real,
   Vec,
   add,
@@ -11,6 +12,10 @@ import {
   fn,
   geq,
   gt,
+  iadd,
+  ieq,
+  ilt,
+  leq,
   lt,
   mul,
   neg,
@@ -38,7 +43,13 @@ const all = (xs: Bool[]): Bool => xs.reduce((a, b) => and(a, b));
 
 const vnot = (xs: Bool[]): Bool[] => xs.map((x) => not(x));
 
-const vsub = (a: Real[], b: Real[]): Real[] => a.map((x, i) => sub(x, b[i]));
+const vadd = (a: Vec<Real> | Real[], b: Vec<Real> | Real[]): Real[] => {
+  return [add(a[0], b[0]), add(a[1], b[1])];
+};
+
+const vsub = (a: Vec<Real> | Real[], b: Vec<Real> | Real[]): Real[] => {
+  return [sub(a[0], b[0]), sub(a[1], b[1])];
+};
 
 const vmul = (v: Real[], c: Real): Real[] => v.map((x) => mul(x, c));
 
@@ -47,13 +58,53 @@ const dot = (a: Real[], b: Real[]): Real =>
 
 const norm = (v: Real[]): Real => sqrt(dot(v, v));
 
-const area = (ab: Real, bc: Real, ca: Real): Real => {
-  const one = add(add(ab, bc), ca);
-  const two = add(add(neg(ab), bc), ca);
-  const three = add(sub(ab, bc), ca);
-  const four = sub(add(ab, bc), ca);
+const cross = (a: Real[], b: Real[]): Real =>
+  sub(mul(a[0], b[1]), mul(a[1], b[0]));
 
-  return mul(0.25, sqrt(mul(mul(mul(one, two), three), four)));
+const reorder = fn([Vec(3, Vec(2, Real))], Vec(5, Vec(2, Real)), (p) => {
+  let i: Nat = 0;
+  for (let j = 1; j < 3; ++j) {
+    const [xi, yi] = p[i] as any;
+    const [xj, yj] = p[j];
+    i = select(or(lt(yj, yi), and(leq(yj, yi), lt(xj, xi))), 3, j, i);
+  }
+  const [a, b, c] = p;
+  return select(
+    ieq(3, i, 0),
+    Vec(5, Vec(2, Real)),
+    [a, b, c, a, b],
+    select(
+      ieq(3, i, 1),
+      Vec(5, Vec(2, Real)),
+      [b, c, a, b, c],
+      [c, a, b, c, a],
+    ),
+  );
+});
+
+// note: only works on triangles
+const minkowskiSum = (
+  left: Vec<Vec<Real>>,
+  right: Vec<Vec<Real>>,
+): Real[][] => {
+  const p = reorder(left);
+  const q = reorder(right);
+  const r: Real[][] = [];
+  let i: Nat = 0;
+  let j: Nat = 0;
+  for (let k = 0; k < 6; ++k) {
+    r.push(vadd(p[i], q[j]));
+    const i1 = iadd(5, i, 1);
+    const j1 = iadd(5, j, 1);
+    const z = cross(vsub(p[i1], p[i]), vsub(q[j1], q[j]));
+    ({ i, j } = select(
+      and(ilt(5, i, 3), geq(z, 0)),
+      { i: 5, j: 5 },
+      { i: i1, j },
+      { i, j: j1 },
+    ));
+  }
+  return r;
 };
 
 // https://iquilezles.org/articles/distfunctions2d/
@@ -76,10 +127,6 @@ const sdPolygon = (v: Real[][], p: Real[]): Real => {
   return mul(s, sqrt(d));
 };
 
-const triangleSum = (p: Real[][], q: Real[][]): Real[][] => {
-  return p; // TODO
-};
-
 type Vec2 = number[];
 
 interface Triangle {
@@ -87,6 +134,21 @@ interface Triangle {
   b: Vec2;
   c: Vec2;
 }
+
+const area = (ab: Real, bc: Real, ca: Real): Real => {
+  const one = add(add(ab, bc), ca);
+  const two = add(add(neg(ab), bc), ca);
+  const three = add(sub(ab, bc), ca);
+  const four = sub(add(ab, bc), ca);
+
+  return mul(0.25, sqrt(mul(mul(mul(one, two), three), four)));
+};
+
+const clockwise = (a: Real[], b: Real[], c: Real[]): Bool =>
+  lt(
+    mul(sub(b[0], a[0]), sub(c[1], a[1])),
+    mul(sub(c[0], a[0]), sub(b[1], a[1])),
+  );
 
 const numTriangles = 10;
 
@@ -132,25 +194,36 @@ const lagrangian = fn(
       mul(
         weight,
         sum(
-          vec(numTriangles, Real, (i) =>
-            sum(
+          vec(numTriangles, Real, (i) => {
+            const a1 = [ax[i], ay[i]];
+            const b1 = [bx[i], by[i]];
+            const c1 = [cx[i], cy[i]];
+            const p = select(
+              clockwise(a1, b1, c1),
+              Vec(3, Vec(2, Real)),
+              [c1, b1, a1],
+              [a1, b1, c1],
+            );
+            return sum(
               vec(numTriangles, Real, (j) => {
-                const diff = triangleSum(
-                  [
-                    [ax[i], ay[i]],
-                    [bx[i], by[i]],
-                    [cx[i], cy[i]],
-                  ],
-                  [
-                    [neg(ax[j]), neg(ay[j])],
-                    [neg(bx[j]), neg(by[j])],
-                    [neg(cx[j]), neg(cy[j])],
-                  ],
+                const a2 = [neg(ax[j]), neg(ay[j])];
+                const b2 = [neg(bx[j]), neg(by[j])];
+                const c2 = [neg(cx[j]), neg(cy[j])];
+                const q = select(
+                  clockwise(a1, b1, c1),
+                  Vec(3, Vec(2, Real)),
+                  [c2, b2, a2],
+                  [a2, b2, c2],
                 );
-                return sqr(max(0, neg(sdPolygon(diff, [0, 0]))));
+                return select(
+                  ieq(numTriangles, i, j),
+                  Real,
+                  0,
+                  sqr(max(0, neg(sdPolygon(minkowskiSum(p, q), [0, 0])))),
+                );
               }),
-            ),
-          ),
+            );
+          }),
         ),
       ),
     );
